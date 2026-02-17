465a1d00d232ea2f6a032736ffd680a35c7447d4
Kayalnilavan <kayalnilavan110@gmail.com>
2026-02-17 23:56:23 +0530

    Add Initial file
    This project demonstrates a basic implementation of Object-Oriented Programming (OOP) concepts in Java using classes and objects. It defines a Pen class with properties and a method, and a MainPen class to create and use a Pen object.

---
+++ 19 JAN 2026/MainPen.java
@@ -0,0 +1,14 @@
+class MainPen{
+	public static void main(String args[]){
+		
+		Pen obj= new Pen();
+		
+		obj.brand="Ten";
+		obj.madeby="Srilanka";
+		obj.color="blue";
+		obj.height=10.7;
+		obj.weight=5.4;
+		
+		obj.write();
+	}
+}
