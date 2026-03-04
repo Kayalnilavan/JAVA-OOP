bd7789b592a052b7233a2aac290db9fc88ff47f8
Kayalnilavan <kayalnilavan110@gmail.com>
2026-03-04 23:36:01 +0530

    Add Default Method file
    This project contains two simple Java files that demonstrate the basics of classes and objects. The `Student3` class defines two attributes, `id` and `name`, along with methods to set default values (`setStudent()`) and display them (`getStudent()`). The `MainStudent3` class includes the `main` method, where an object of `Student3` is created, initialized, and its details are printed to the console. Running the program outputs the student’s ID and name, showing how object-oriented programming in Java can be used to encapsulate data and behavior in a class.

---
+++ 2026 JAN 21/Default Method/MainStudent3.java
@@ -0,0 +1,9 @@
+class MainStudent3{
+	public static void main(String args[]){
+		
+		Student3 st= new Student3();
+		
+		st.setStudent();
+		st.getStudent();
+	}
+}
---
+++ 2026 JAN 21/Default Method/Student3.java
@@ -0,0 +1,15 @@
+class Student3{
+	
+	int id;
+	String name;
+		
+	void setStudent(){
+		id=1001;
+		name="Raj";
+	}
+	
+	void getStudent(){
+		System.out.println("Student ID: "+ id);
+		System.out.println("Student Name: "+ name);
+	}
+}
