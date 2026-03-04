fee82465dc5e650a2bfa766ae6832d9db8e794fe
Kayalnilavan <kayalnilavan110@gmail.com>
2026-03-04 15:46:22 +0530

    Add Student file
    This Java project demonstrates basic Object-Oriented Programming (OOP) concepts using a Student class and a MainStudent class. The Student class contains two attributes, id and name, along with two methods: setStudent() to assign values to the student details and getStudent() to display them. In the MainStudent class, a Student object is created, the setStudent() method is called to initialize the data, and the getStudent() method is used to print the student information to the console. This project is designed for beginners to understand how classes, objects, and methods work in Java.

---
+++ 2026 JAN 20/Student.java
@@ -0,0 +1,13 @@
+class Student{	
+	int id;
+	String name;
+	
+	void setStudent(){
+		id=1001;
+		name="Kayal";
+	}	
+	void getStudent(){
+		System.out.println("Student ID: "+ id);
+		System.out.println("Student Name: "+ name);
+	}
+}
