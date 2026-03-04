c091d96da999165d459127c14ca5cf60fb3223f5
Kayalnilavan <kayalnilavan110@gmail.com>
2026-03-04 23:44:12 +0530

    Add Parameter Method file
    This project showcases a simple Java program using classes and objects to manage student details. The `Student2` class defines attributes for ID and name, with methods to set values through parameters (`setStudent()`) and display them (`getStudent()`). The `MainStudent2` class creates a `Student2` object, assigns an ID and name, and prints the student’s information. Running the program demonstrates how parameterized methods and the `this` keyword can be used to handle and display object data in Java.

---
+++ 2026 JAN 21/Parameter Method/MainStudent2.java
@@ -0,0 +1,9 @@
+class MainStudent2{
+	public static void main(String args[]){
+		
+		Student2 st= new Student2();
+		
+		st.setStudent(3300,"Kayal");
+		st.getStudent();
+	}
+}
---
+++ 2026 JAN 21/Parameter Method/Student2.java
@@ -0,0 +1,15 @@
+class Student2{
+	
+	int id;
+	String name;
+		
+	void setStudent(int id,String name){
+		this.id=id;
+		this.name="name";
+	}
+	
+	void getStudent(){
+		System.out.println("Student ID: "+ this.id);
+		System.out.println("Student Name: "+ this.name);
+	}
+}
