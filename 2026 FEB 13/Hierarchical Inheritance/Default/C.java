3e800cd08da2ba4b1fc1a36db7b6d967ee03d16c
Kayalnilavan <kayalnilavan110@gmail.com>
2026-03-05 00:53:58 +0530

    Add Default Hierarchical Inheritance
    This project demonstrates a Java program that uses hierarchical inheritance, where multiple classes extend a single parent class. The `A` class initializes an attribute `x` and provides a method to display it. The `B` class extends `A`, adds its own attribute `y`, and includes a method to display it, while the `C` class also extends `A`, introduces an attribute `z`, and provides a method to display it. The `MainABC` class creates objects of both `B` and `C`, showing how subclasses can inherit methods and attributes from the parent class while maintaining their own unique properties. Running the program highlights the concept of hierarchical inheritance in Java.

---
+++ 2026 FEB 13/Hierarchical Inheritance/Default/C.java
@@ -0,0 +1,9 @@
+class C extends A{
+	int z;
+	C(){
+		this.z=30;
+	}
+	void getZ(){
+		System.out.println("Cz "+this.z);
+	}
+}
---
+++ 2026 FEB 13/Hierarchical Inheritance/Default/MainABC.java
@@ -0,0 +1,9 @@
+class MainABC{
+	public static void main(String agrs[]){
+		B objb = new B();
+		objb.getY();
+		objb.getX();
+		C objc = new C();
+		objc.getZ();
+	}
+}
